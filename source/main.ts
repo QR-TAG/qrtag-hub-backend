d3ae955be1e0c32109f99297353de69e8d5fd608
euberdeveloper <euberdeveloper@gmail.com>
2022-06-23 14:28:23 +0200

    feat: go on
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/main.ts
+++ source/main.ts
@@ -10,15 +10,46 @@ const io = new Server(httpServer, {
 
 io.on('connection', socket => {
     socket.data.guns = [];
+    socket.data.users = [];
 
-    socket.on('startGame', () => {
-        socket.emit('gameStarted', socket.data.guns);
+    socket.on('startSetup', () => {
+        socket.emit('setupStarted', socket.data.guns);
     });
 
     socket.on('announceGun', (data: { id: string }) => {
         socket.data.guns.push(data.id);
         console.log(socket.data.guns);
     });
+
+    socket.on('addUser', (data: { gunId: string, username: string }) => {
+        socket.data.users.push({
+            gunId: data.gunId,
+            username: data.username,
+            tshirtId: null,
+            life: 5
+        });
+    });
+
+    socket.on('announceTshirt', (data: { gunId: string, tshirtId: string }) => {
+        socket.data.users = socket.data.users.map((user: any) => {
+            if (user.gunId === data.gunId) {
+                user.tshirtId = data.tshirtId;
+            }
+            return user;
+        });
+    });
+
+    socket.on('tshirtShot', (data: { shooterGunId: string, tshirtId: string }) => {
+        const user = socket.data.users.find((user: any) => user.tshirtId === data.tshirtId);
+        if (user) {
+            user.life--;
+        }
+    });
+
+    socket.on('startGame', () => {
+
+    });
+
 });
 
 httpServer.listen(3000);
